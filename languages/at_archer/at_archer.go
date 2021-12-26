52c2d05e66a5331da7a381c06bdddbb9ac133ee8
s_tanaka <s05.t12.030@gmail.com>
2021-12-26 23:07:55 +0900

    Fix tab space

--- languages/at_archer/at_archer.go
+++ languages/at_archer/at_archer.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"bufio"
+    "bufio"
     "fmt"
-	"os"
-	"strconv"
+    "os"
+    "strconv"
 )
 
 var sc = bufio.NewScanner(os.Stdin)
@@ -21,39 +21,39 @@ func nextInt() int {
 func main() {
     sc.Split(bufio.ScanWords)
     n := nextInt()
-	m := nextInt()
-	d := nextInt()
-	r := make([]int, m + 1)
+    m := nextInt()
+    d := nextInt()
+    r := make([]int, m + 1)
     for i := 0; i < m + 1; i++ {
         r[i] = nextInt()
     }
-	s := make([]int, m)
-	for i := 0; i < m; i++ {
+    s := make([]int, m)
+    for i := 0; i < m; i++ {
         s[i] = nextInt()
     }
     fmt.Println(solve(n, m, d, r, s))
 }
 
 func solve(n int, m int, d int, r []int, s []int) int {
-	a := -(n * d / 2)
-	tmp_sl := make([]int, 2 * m)
-	for i := 0; i < m; i++ {
+    a := -(n * d / 2)
+    tmp_sl := make([]int, 2 * m)
+    for i := 0; i < m; i++ {
         tmp_sl[i] = -r[m - i]
     }
-	for i := 1; i <= m; i++ {
+    for i := 1; i <= m; i++ {
         tmp_sl[m + i - 1] = r[i] + 1
     }
-	l := tmp_sl[:len(tmp_sl) - 1]
-	r_sl := tmp_sl[1:]
-	s_sl := make([]int, 2 * m - 1)
-	for i := 0; i < m; i++ {
+    l := tmp_sl[:len(tmp_sl) - 1]
+    r_sl := tmp_sl[1:]
+    s_sl := make([]int, 2 * m - 1)
+    for i := 0; i < m; i++ {
         s_sl[i] = s[m - i - 1]
     }
-	for i := 1; i < m; i++ {
+    for i := 1; i < m; i++ {
         s_sl[m + i - 1] = s[i]
     }
-	imos := make([]int, d + 1)
-	for i := 0; i < 2 * m - 1; i++ {
+    imos := make([]int, d + 1)
+    for i := 0; i < 2 * m - 1; i++ {
         if l[i] - a >= n * d {
             imos[0] -= n * s_sl[i]
             imos[d] += n * s_sl[i]
@@ -71,13 +71,13 @@ func solve(n int, m int, d int, r []int, s []int) int {
             imos[d] -= (r_sl[i] - a) / d * s_sl[i]
         }
     }
-	val := 0
-	score := 0
-	for i := 0; i < d; i++ {
+    val := 0
+    score := 0
+    for i := 0; i < d; i++ {
         val += imos[i]
-		if val > score {
-			score = val
-		}
-	}
-	return score
+        if val > score {
+            score = val
+        }
+    }
+    return score
 }
